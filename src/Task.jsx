d646ad5d066deb4325e0e00e69a054adbdcc7aae
Trần Nguyễn Khánh Duy <71444587+santaduytran@users.noreply.github.com>
2024-10-11 20:39:06 +0700

    Update Task.jsx

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 
-const Task = ({ task, toggleTask }) => {
+const Task = ({ task, toggleTask, deleteTask, showDeleteButton }) => {
   return (
     <div className="task">
       <input type="checkbox" checked={!task.active} onChange={toggleTask} />
       <span style={{ textDecoration: task.active ? "none" : "line-through" }}>
         {task.text}
       </span>
+
+      {/* Show delete button only if it's in the Completed tab */}
+      {showDeleteButton && (
+        <button onClick={deleteTask} className="delete-btn">
+          Delete
+        </button>
+      )}
     </div>
   );
 };
